fb0a4acb94b637b50c7b20d863cc60beba376972
Mori-mushroom <icepaw99@gmail.com>
2023-10-02 13:48:27 -0600

    first commit being made

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module promineoTech {
+}
---
+++ src/promineoTech/week3Content.java
@@ -0,0 +1,11 @@
+package promineoTech;
+
+public class week3Content {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Testing");
+	}
+
+}
